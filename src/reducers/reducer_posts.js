0efce756d167b57589fb1f48e0cff86b1528251e
DESKTOP-LU0C24T\cuong <bacuongtr@gmail.com>
2017-03-31 08:02:34 +0700

    delete post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS,FETCH_POST } from '../actions/index';
+import { FETCH_POSTS,FETCH_POST, DELETE_POST } from '../actions/index';
 
 const INITIAL_STATE = { all: [], post: null };
 
@@ -7,7 +7,7 @@ export default function (state = INITIAL_STATE, action) {
         case FETCH_POSTS:
             return { ...state, all: action.payload.data };
         case FETCH_POST:
-            return {...state, post:action.payload.data}         
+            return {...state, post:action.payload.data};
         default:
             return state;
     }
